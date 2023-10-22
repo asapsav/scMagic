f35c82d78cfb39bd30776b093bb4320adb30763f
Saveliy Kholin <holins12@gmail.com>
2023-10-22 09:26:10 -0700

    delete

---
+++ cases/case_0_breast_cancer_t_cell/links_to_cases.py
@@ -0,0 +1,11 @@
+LINKS_TO_CASES = {
+    2: {'comment': "sc clustering", 
+          'link': "https://github.com/genepattern/single_cell_clustering_notebook/tree/master"
+          },
+    0: {'comment': "breast cancer t cell",
+            'link': ""},
+    1: {'comment': "mouse intestinal epithelium",
+            'link': ""},
+    
+
+}
